b6aabea8ef15f3e467758a4457a25eb87b8b5a3c
wilker ferreira <wilkerferreira818@gmail.com>
2023-09-14 17:59:06 -0300

    feat: salvando e recuperando clientes

--- Spring Boot Expert_JPA_RESTFull_Security_JWT/vendas/src/main/java/io/github/wilker818/VendasApplication.java
+++ Spring Boot Expert_JPA_RESTFull_Security_JWT/vendas/src/main/java/io/github/wilker818/VendasApplication.java
@@ -1,13 +1,31 @@
 package io.github.wilker818;
 
+import io.github.wilker818.domain.entity.Cliente;
+import io.github.wilker818.domain.repositorio.Clientes;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.context.annotation.Bean;
+
+import java.util.List;
 
 @SpringBootApplication
-@RestController
 public class VendasApplication {
+
+
+    @Bean
+    public CommandLineRunner init(@Autowired Clientes clientes){
+        return args -> {
+            clientes.salvar(new Cliente("Milker"));
+            clientes.salvar(new Cliente("Outro Cliente"));
+
+            List<Cliente> todosClientes = clientes.obterTodos();
+            todosClientes.forEach(System.out::println);
+        };
+    }
+
+
     public static void main(String[] args) {
         SpringApplication.run(VendasApplication.class, args);
     }
